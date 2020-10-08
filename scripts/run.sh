99f8ffbfa3f279c9d15864ed51ee7d91da8291f0
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-10-08 20:56:50 +0100

    add watchdog to scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-# declare -a ACTIVE_MODULES=("raiIn" "raiOut" "ahrs" "air" "psu" "ctrl" "sFusion" "downlink")
-declare -a ACTIVE_MODULES=("raiIn" "raiOut" "ahrs" "air" "psu" "ctrl" "sFusion")
+declare -a ACTIVE_MODULES=("raiIn" "raiOut" "ahrs" "air" "psu" "ctrl" "sFusion" "downlink" "watchdog")
 
 MODULE_DIR=./../modules
 BUILD_DIR=/build
