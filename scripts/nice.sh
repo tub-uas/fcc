2cafd61d807a4bdca2254f42cfa7ff1905ceeb23
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-10-12 18:07:34 +0100

    nice script

---
+++ scripts/nice.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+declare -a ACTIVE_MODULES=("raiIn" "ahrs" "air" "psu")
+
+BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+
+echo "----- Running modules on branch $BRANCH ------"
+
+for mod in "${ACTIVE_MODULES[@]}"
+do
+	PID=$(pgrep $mod)
+	echo "sudo renice -n -20 -p $PID"
+	      sudo renice -n -20 -p $PID
+done
+
+echo "----- `basename "$0"` done on branch $BRANCH ------"
+
+# ip -d -s link show can0
+# sysbench --test=cpu --cpu-max-prime=1000000 --num-threads=8 run
