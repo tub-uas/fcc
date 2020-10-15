cf86791ef2df0665a89380ffa0fe73d828a6df78
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-10-15 10:32:52 +0100

    increase delay in watchdog

--- modules/watchdog/watchdog.cpp
+++ modules/watchdog/watchdog.cpp
@@ -528,8 +528,8 @@ void Watchdog::run() {
 
 		greenLed = allGood;
 
-		static auto next_wakeup = std::chrono::steady_clock::now() + std::chrono::milliseconds(1);
+		static auto next_wakeup = std::chrono::steady_clock::now() + std::chrono::milliseconds(10);
 		std::this_thread::sleep_until(next_wakeup);
-		next_wakeup += std::chrono::milliseconds(1);
+		next_wakeup += std::chrono::milliseconds(10);
 	}
 }
