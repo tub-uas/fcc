b158a0b636ca3fdf3d6baf5f9027e60694070f8a
fabian <fabianpedd@gmail.com>
2020-10-27 18:14:22 +0100

    log download script

---
+++ scripts/getLogs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+
+echo "----- Checking Wifi devices on branch $BRANCH ------"
+
+# dt=$(date '+%Y-%m-%d-%H-%M-%S');
+# echo "----- Current date $dt -----"
+#
+# mkdir ../old-logs
+# mv ../logs ../old-logs/logs-$dt
+
+scp -r pi@raspberrypi.local:/home/pi/fcc/logs/ /home/fabian/tub-uas/fcc/logs
+
+echo "----- `basename "$0"` done on branch $BRANCH ------"
